cbc6db71aa8a9208e19ba02f8046af0bca597172
RAKOTOSON MALALANIRINA Tahina Lauren <rakotosonmalal@et.esiea.fr>
2023-05-31 17:09:44 +0200

    update 31/05

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
+
+
+
+var map = L.map('map').setView([48.836961, 2.536974], 13);
+
+// Ajoute une couche de tuiles (carte de base)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+                
+// Ajoute un marqueur à l'emplacement spécifié
+L.marker([48.836961, 2.536974]).addTo(map);
+
+
+
+const galleryImages = document.querySelectorAll('.gallery-image');
+
+galleryImages.forEach((image) => {
+  image.addEventListener('click', () => {
+    image.classList.toggle('large');
+  });
+});
